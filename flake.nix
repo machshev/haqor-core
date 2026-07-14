74dc03f2dcac5d87d9e572da25f22a5912ebba48
David James McCorrie <james.mccorrie@lowrisc.org>
2026-07-14 23:56:04 +0100

    feat: package haqor workspace with nix

--- flake.nix
+++ flake.nix
@@ -12,7 +12,7 @@
     with inputs;
       flake-utils.lib.eachDefaultSystem (
         system: let
-          overlays = [ (import rust-overlay) ];
+          overlays = [(import rust-overlay)];
           pkgs = import nixpkgs {
             inherit system overlays;
           };
@@ -20,45 +20,83 @@
           # SemVer bump helper: bump-version <major|minor|patch|X.Y.Z> [--tag]
           bump-version = pkgs.writeShellApplication {
             name = "bump-version";
-            runtimeInputs = with pkgs; [ gnugrep gnused gawk coreutils git ];
+            runtimeInputs = with pkgs; [gnugrep gnused gawk coreutils git];
             text = builtins.readFile ./scripts/bump-version.sh;
           };
 
-        in {
+          # Keep the CLI installable on its own, while also exposing the
+          # workspace crates as buildable package outputs for downstream
+          # flakes.
+          mkHaqorCrate = crate: pname:
+            pkgs.rustPlatform.buildRustPackage {
+              inherit pname;
+              version = "0.6.1";
+              src = ./.;
+              cargoLock.lockFile = ./Cargo.lock;
+              cargoBuildFlags = ["-p" crate];
+              cargoTestFlags = ["-p" crate];
+            };
 
-          packages.bump-version = bump-version;
+          haqor = mkHaqorCrate "haqor-cli" "haqor";
+          sync-server = pkgs.writeShellApplication {
+            name = "haqor-sync-server";
+            runtimeInputs = [haqor];
+            text = ''
+              exec haqor sync-server "$@"
+            '';
+          };
+        in {
+          packages = {
+            inherit bump-version haqor sync-server;
+            haqor-cli = haqor;
+            haqor-admin = mkHaqorCrate "haqor-admin" "haqor-admin";
+            haqor-core = mkHaqorCrate "haqor-core" "haqor-core";
+            haqor-db-gen = mkHaqorCrate "haqor-db-gen" "haqor-db-gen";
+            haqor-morphology = mkHaqorCrate "haqor-morphology" "haqor-morphology";
+          };
 
           apps.bump-version = {
             type = "app";
             program = "${bump-version}/bin/bump-version";
           };
 
+          apps.haqor = {
+            type = "app";
+            program = "${haqor}/bin/haqor";
+          };
+
+          apps.sync-server = {
+            type = "app";
+            program = "${sync-server}/bin/haqor-sync-server";
+          };
+
           devShells = {
-            default = with pkgs; mkShell {
-              buildInputs = [
-                (rust-bin.selectLatestNightlyWith (toolchain: toolchain.default))
-                cargo-nextest
-                cargo-udeps
-                cargo-vet
-                cargo-about
-                cargo-release
+            default = with pkgs;
+              mkShell {
+                buildInputs = [
+                  (rust-bin.selectLatestNightlyWith (toolchain: toolchain.default))
+                  cargo-nextest
+                  cargo-udeps
+                  cargo-vet
+                  cargo-about
+                  cargo-release
 
-                bump-version
+                  bump-version
 
-                rust-analyzer
-                rustfmt
+                  rust-analyzer
+                  rustfmt
 
-                adrs
-                typos
+                  adrs
+                  typos
 
-                sqlitebrowser
+                  sqlitebrowser
 
-                # If the dependencies need system libs, you usually need pkg-config + the lib
-                pkg-config
-                openssl
-                sqlite
-              ];
-            };
+                  # If the dependencies need system libs, you usually need pkg-config + the lib
+                  pkg-config
+                  openssl
+                  sqlite
+                ];
+              };
           };
 
           formatter = nixpkgs.legacyPackages.${system}.alejandra;
